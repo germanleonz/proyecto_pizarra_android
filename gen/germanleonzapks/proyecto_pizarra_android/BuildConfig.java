baf4daa841a233e8e5cded308aacb84121a5b888
German Leon Zapata <germanleonz@gmail.com>
2013-03-25 21:37:28 -0430

    Primer commit de la version Android de la aplicacion proyecto pizarra

---
+++ gen/germanleonzapks/proyecto_pizarra_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package germanleonzapks.proyecto_pizarra_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
