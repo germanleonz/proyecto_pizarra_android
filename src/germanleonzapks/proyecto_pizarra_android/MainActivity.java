0f9fa094604dbb2e84ef7a71ebf6220bc93ff57a
German Leon Zapata <germanleonz@gmail.com>
2013-03-28 12:46:59 -0430

    Avances generales en las vistas etc

--- src/germanleonzapks/proyecto_pizarra_android/MainActivity.java
+++ src/germanleonzapks/proyecto_pizarra_android/MainActivity.java
@@ -1,15 +1,25 @@
 package germanleonzapks.proyecto_pizarra_android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "MainActivity";
+	private Button loginButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		loginButton = (Button) findViewById(R.id.button_login);
+		loginButton.setOnClickListener(new MyButtonClickListener());
 	}
 
 	@Override
@@ -19,4 +29,19 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class MyButtonClickListener implements OnClickListener {
+		public void onClick(View v) {
+			Intent i;
+			switch (v.getId()) {
+				case R.id.button_login:
+					i = new Intent(MainActivity.this, LoginActivity.class);
+					startActivity(i);
+					break;
+				case R.id.button_sign_in:
+					i = new Intent(MainActivity.this, RegistrationActivity.class);
+					startActivity(i);
+					break;
+			}
+		}
+	}
 }
